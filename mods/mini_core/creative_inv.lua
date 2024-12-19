38fbafe2275ad66d3c773c886b73e2a4b93b4bb6
danil275487 <danilsirin24@gmail.com>
2024-12-19 07:37:39 +0300

    gghhw

--- mods/mini_core/creative_inv.lua
+++ mods/mini_core/creative_inv.lua
@@ -18,11 +18,11 @@ function mini_core.get_creative_formspec(page)
 		formspec_version[8]
 		size[8.25,9.25]
 		list[detached:creative;main;0.5,1.25;5,5;${start}]
-		button[7,1.25;0.75,0.75;inv_creative_prev;\/\\]
-		background9[7,2.5;0.5,4.5;${bg_dark};false;8]
+		button[6.75,1.25;1,1;inv_creative_prev;\/\\]
+		background9[6.75,2.25;1,4;${bg_dark};false;8]
 		label[0,1;${scrubber_pos}]
-		background9[7,${scrubber_pos}; 0.75,0.75;${bg};false;12]
-		button[7,6.25;0.75,0.75;inv_creative_next;\\\/]
+		background9[6.75,${scrubber_pos}; 1,0.75;${bg};false;12]
+		button[6.75,6.25;1,1;inv_creative_next;\\\/]
 		list[current_player;main;0.5,7.75;6,1;0]
 		field[0,0;0,0;internal_paginator;;${page}]
 		background9[0,0;8.25,0.75;${bg_dark};false;8]
@@ -34,7 +34,7 @@ function mini_core.get_creative_formspec(page)
 		start = start,
 		page = page,
 		max_page = max_page,
-		scrubber_pos = 4.5*(page-1)/(max_page-1)
+		scrubber_pos = 2.25+(5.5-2.25)*(page-1)/(max_page-1)
 	})
 end
 
