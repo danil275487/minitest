70c3078483705b6b4a14e2a1e7e1681ebdee7e4d
danil275487 <danilsirin24@gmail.com>
2025-03-19 01:50:31 +0300

    boom i broke your creative fuck y

--- mods/mini_core/formspecs.lua
+++ mods/mini_core/formspecs.lua
@@ -1,15 +1,16 @@
 mini_core.formspecs = {}
 
+local bg = mini_core.sheet("ui",0,0,6,1).."^[resize:24x24"
+local bg_dark = mini_core.sheet("ui",2,0,6,1).."^[resize:24x24"
+local button_active = mini_core.sheet("ui",1,0,6,1).."^[resize:24x24"
+
 --global formspec prepend
 function mini_core.formspecs.prepend()
-	return mini_core.formspec_wrapper([[
-		background9[0,0;0,0;${bg};true;8]
-		listcolors[#787878ff;#505050ff;#787878ff;#7869c4ff;#ffffffff]
-		style_type[button:default;border=false;bgimg=${bg};bgimg_middle=8,8]
-		style_type[button:pressed;border=false;bgimg=${btn_active};bgimg_middle=8,8]
-	]], {
-		bg = mini_core.sheet("ui",0,0,6,1).."^[resize:24x24",
-		btn_active = mini_core.sheet("ui",1,0,6,1).."^[resize:24x24"
+	return fslib.build_formspec({
+		{"background9", {0,0}; {0,0}; bg, true, 8},
+		{"listcolors", "#787878ff", "#505050ff", "#00000000", "#7869c4ff", "#ffffffff"},
+		{"style_type", "button:default"; "border=false"; "bgimg="..bg; "bgimg_middle=8,8"},
+		{"style_type", "button:pressed"; "border=false"; "bgimg="..bg_dark; "bgimg_middle=8,8"}
 	})
 end
 
@@ -19,7 +20,7 @@ function mini_core.formspecs.inventory(player)
 	if core.is_creative_enabled(player) then
 		sub = 0
 		creative_buttons = {
-			{"background9", {0,0}; {8.25,0.75}; mini_core.sheet("ui",2,0,6,1).."^[resize:24x24", false, 8},
+			{"background9", {0,0}; {8.25,0.75}; bg_dark, false, 8},
 			{"button", {0,0}; {3,0.75}; "inventory", "Inventory"},
 			{"button", {3,0}; {3,0.75}; "creative", "Creative"},
 		}
@@ -29,7 +30,6 @@ function mini_core.formspecs.inventory(player)
 		{"size", {8.25, 8.25-sub}},
 		{"list", "current_player", "main", {0.5, 4-sub}; {6, 2}; 6},
 		{"list", "current_player", "main", {0.5, 6.75-sub}; {6, 1}},
-		{"listring"},
 		{"list", "current_player", "craft", {1.75, 1.25-sub}; {2, 1}},
 		{"list", "current_player", "craft", {1.75, 2.50-sub}; {2, 1}; 3},
 		{"image", {4.25, 1.875-sub}; {1, 1}; "mini_inv_arrow.png^[transformR270"},
@@ -40,27 +40,20 @@ end
 
 function mini_core.formspecs.creative(page, max_page, ipp)
 	local start = 0 + (page-1)*ipp
-	return mini_core.formspec_wrapper([[
-		formspec_version[8]
-		size[8.25,9.25]
-		list[detached:creative;main;0.5,1.25;5,5;${start}]
-		button[6.75,1.25;1,1;inv_creative_prev;\/\\]
-		background9[6.75,2.25;1,4;${bg_dark};false;8]
-		label[0,1;${scrubber_pos}]
-		background9[6.75,${scrubber_pos}; 1,0.75;${bg};false;12]
-		button[6.75,6.25;1,1;inv_creative_next;\\\/]
-		list[current_player;main;0.5,7.75;6,1;0]
-		field[0,0;0,0;internal_paginator;;${page}]
-		background9[0,0;8.25,0.75;${bg_dark};false;8]
-		button[0,0;3,0.75;inventory;Inventory]
-		button[3,0;3,0.75;creative;Creative]
-	]], {
-		bg = mini_core.sheet("ui",0,0,6,1).."^[resize:24x24",
-		bg_dark = mini_core.sheet("ui",2,0,6,1).."^[resize:24x24",
-		start = start,
-		page = page,
-		max_page = max_page,
-		scrubber_pos = 2.25+(5.5-2.25)*(page-1)/(max_page-1)
+	local scrubber_pos = 2.25+(5.5-2.25)*(page-1)/(max_page-1)
+	return fslib.build_formspec({
+		{"formspec_version", 8},
+		{"size", {8.25, 9.25}},
+		{"list", "detached:creative", "main", {0.5, 1.25}; {5, 5}, start},
+		{"list", "current_player", "main", {0.5, 7.75}; {6, 1}},
+		{"listring"},
+		{"button", {6.75, 1.25}; {1, 1}; "inv_creative_prev", "/\\"},
+		{"button", {6.75, 6.25}; {1, 1}; "inv_creative_next", "\\/"},
+		{"background9", {6.75, 2.25}; {1,4}; bg_dark, false, 8},
+		{"background9", {6.75, scrubber_pos}; {1,0.75}; bg, false, 8},
+		{"background9", {0,0}; {8.25,0.75}; bg_dark, false, 8},
+		{"button", {0,0}; {3,0.75}; "inventory", "Inventory"},
+		{"button", {3,0}; {3,0.75}; "creative", "Creative"},
 	})
 end
 
